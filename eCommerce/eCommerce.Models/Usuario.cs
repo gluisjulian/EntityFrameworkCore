20b5a77c1951c01ab0cee3eaac5d62ff07284239
gluisjulian <gluisjulian@gmail.com>
2024-08-23 19:47:52 -0300

    Criação das Classes.

--- eCommerce/eCommerce.Models/Usuario.cs
+++ eCommerce/eCommerce.Models/Usuario.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace eCommerce.Models
+﻿namespace eCommerce.Models
 {
     public class Usuario
     {
@@ -17,11 +11,10 @@ namespace eCommerce.Models
         public string? NomeMae { get; set; } 
         public string? SituacaoCadastro { get; set; }
         public DateTimeOffset DataCadastro { get; set; }
-
-        //TODO
-        //Vincular com as classes
-        // - Contato
-        // - Endereco Entrega
-        // - Departamento
+        
+        //Relacionamento
+        public Contato? Contato { get; set; }
+        public ICollection<EnderecoEntrega>? EnderecosEntrega { get; set; }
+        public ICollection<Departamento>? Departamentos { get; set; }
     }
 }
