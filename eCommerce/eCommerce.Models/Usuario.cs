1a4c31eabb74d4ed6f5c4dfdf8811fafe9af7a79
gluisjulian <gluisjulian@gmail.com>
2024-08-22 22:54:45 -0300

    Criação da classe Usuario

---
+++ eCommerce/eCommerce.Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; } 
+        public string? Email { get; set; } 
+        public string? Sexo { get; set; } 
+        public string? Rg { get; set; } 
+        public string? Cpf { get; set; } 
+        public string? NomeMae { get; set; } 
+        public string? SituacaoCadastro { get; set; }
+        public DateTimeOffset DataCadastro { get; set; }
+
+        //TODO
+        //Vincular com as classes
+        // - Contato
+        // - Endereco Entrega
+        // - Departamento
+    }
+}
