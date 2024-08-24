eab93a79e493ca25494c2e8ef8fe0d3d2f947f5f
gluisjulian <gluisjulian@gmail.com>
2024-08-23 23:42:48 -0300

    Criação do Repositorio.

---
+++ eCommerce/eCommerce.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,35 @@
+﻿using eCommerce.Models;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        public static List<Usuario> _db = new List<Usuario>();
+
+        public List<Usuario> Get()
+        {
+            return _db;
+        }
+
+        public Usuario GetById(int id)
+        {
+            return _db.Find(x => x.Id == id);
+        }
+
+        public void Add(Usuario usuario)
+        {
+            _db.Add(usuario);
+        }
+
+        public void Update(Usuario usuario)
+        {
+            _db.Remove(GetById(usuario.Id));
+            _db.Add(usuario);
+        }
+
+        public void Delete(int id)
+        {
+            _db.Remove(GetById(id));
+        }
+    }
+}
