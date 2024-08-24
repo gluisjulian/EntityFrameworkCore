b892854fdb3de9e2a58979d3baa056c9aca6c917
gluisjulian <gluisjulian@gmail.com>
2024-08-23 22:18:54 -0300

    Contruindo a interface Usuario

---
+++ eCommerce/eCommerce.API/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using eCommerce.Models;
+
+namespace eCommerce.API.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        List<Usuario> Get();
+        Usuario GetById(int id);
+        void Add(Usuario usuario);
+        void Update(Usuario usuario);
+        void Delete(int id);
+    }
+}
