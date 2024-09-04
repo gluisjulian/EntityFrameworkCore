a20c6c9bba401eaee40c3ae150efc65367729121
gluisjulian <gluisjulian@gmail.com>
2024-09-04 19:14:13 -0300

    Criação da UsuarioController

---
+++ eCommerce/eCommerce.API/Controllers/UsuariosController.cs
@@ -0,0 +1,57 @@
+﻿using eCommerce.API.Repositories;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.API.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly IUsuarioRepository _repository;
+        public UsuariosController(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var listaUsuarios = _repository.Get();
+            return Ok(listaUsuarios);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id) 
+        {
+            var usuario = _repository.GetById(id);
+            if (usuario == null)
+                return NotFound("Usuario nao encontrado!");
+
+            return Ok(usuario);
+
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Usuario usuario)
+        {
+            _repository.Add(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody] Usuario usuario, int id)
+        {
+            _repository.Update(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        { 
+            _repository.Delete(id);
+            return Ok();
+        }
+
+    }
+}
