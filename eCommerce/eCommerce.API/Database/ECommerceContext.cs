e60444a3c30e61e7271c5b14abaf8aa17c9cb20d
gluisjulian <gluisjulian@gmail.com>
2024-09-11 20:38:30 -0300

    Criando conexao no OnConfigure

---
+++ eCommerce/eCommerce.API/Database/ECommerceContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.API.Database
+{
+    public class ECommerceContext : DbContext
+    {
+        //public ECommerceContext(DbContextOptions<ECommerceContext> options) : base(options)
+        //{
+
+        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;");
+        }
+    }
+}
